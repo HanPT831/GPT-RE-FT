4d2d69b50198e48a42c97606d180e2099cdeabbd
HanPT831 <129965456+HanPT831@users.noreply.github.com>
2025-12-01 17:27:07 +0900

    Delete REbaseline/scripts/run_bert_base_tbd.sh

--- REbaseline/scripts/run_bert_base_tbd.sh
+++
@@ -1,3 +0,0 @@
-for SEED in 100;
-do python train_tbd.py --model_name_or_path bert-base-cased  --output_dir ./models/TB-Dense --input_format entity_marker --num_train_epochs 10.0 --seed $SEED --train_batch_size 8 --test_batch_size 32 --learning_rate 3e-5 --gradient_accumulation_steps 1 --run_name bert-tbd;
-done;
